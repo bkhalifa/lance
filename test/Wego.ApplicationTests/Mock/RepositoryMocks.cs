4d28e4823e7837d60eb6b946d358347a13bc757d
BILELBEN <benkhalifa.bilel@hotmail.fr>
2023-05-01 20:51:59 +0200

    removed unused code

--- test/Wego.ApplicationTests/Mock/RepositoryMocks.cs
+++ test/Wego.ApplicationTests/Mock/RepositoryMocks.cs
@@ -15,10 +15,6 @@ namespace Wego.ApplicationTests.Mock
     {
         public static Mock<IAsyncRepository<Category>> GetCategoryRepository()
         {
-            var concertGuid = Guid.Parse("{B0788D2F-8003-43C1-92A4-EDC76A7C5DDE}");
-            var musicalGuid = Guid.Parse("{6313179F-7837-473A-A4D5-A5571B43E6A6}");
-            var playGuid = Guid.Parse("{BF3F3002-7E53-441E-8B76-F6280BE284AA}");
-            var conferenceGuid = Guid.Parse("{FE98F549-E790-4E9F-AA16-18C2292A2EE9}");
 
             var categories = new List<Category>
             {
