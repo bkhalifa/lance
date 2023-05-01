2d4ba02f18affb7aba1e565f175714a95f1aedff
BILELBEN <benkhalifa.bilel@hotmail.fr>
2023-05-01 20:47:57 +0200

    structure projects and tests

---
+++ test/Wego.ApplicationTests/Mock/RepositoryMocks.cs
@@ -0,0 +1,60 @@
+﻿using Moq;
+
+using Wego.Application.Contracts.Persistence;
+using Wego.Domain.Entities;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wego.ApplicationTests.Mock
+{
+    public class RepositoryMocks
+    {
+        public static Mock<IAsyncRepository<Category>> GetCategoryRepository()
+        {
+            var concertGuid = Guid.Parse("{B0788D2F-8003-43C1-92A4-EDC76A7C5DDE}");
+            var musicalGuid = Guid.Parse("{6313179F-7837-473A-A4D5-A5571B43E6A6}");
+            var playGuid = Guid.Parse("{BF3F3002-7E53-441E-8B76-F6280BE284AA}");
+            var conferenceGuid = Guid.Parse("{FE98F549-E790-4E9F-AA16-18C2292A2EE9}");
+
+            var categories = new List<Category>
+            {
+                new Category
+                {
+                   Id = 1,
+                    Name = "Concerts"
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Musicals"
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Conferences"
+                },
+                 new Category
+                {
+                    Id = 3,
+                    Name = "Plays"
+                }
+            };
+
+            var mockCategoryRepository = new Mock<IAsyncRepository<Category>>();
+            mockCategoryRepository.Setup(repo => repo.ListAllAsync()).ReturnsAsync(categories);
+
+            mockCategoryRepository.Setup(repo => repo.AddAsync(It.IsAny<Category>())).ReturnsAsync(
+                (Category category) =>
+                {
+                    categories.Add(category);
+                    return category;
+                });
+
+            return mockCategoryRepository;
+        }
+    }
+}
