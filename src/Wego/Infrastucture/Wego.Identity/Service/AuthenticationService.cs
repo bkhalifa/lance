c3bd133d825a09a4f5a9fcb7fa17b06f821163a2
BILELBEN <benkhalifa.bilel@hotmail.fr>
2023-05-03 20:07:33 +0200

    merge

--- src/Wego/Infrastucture/Wego.Identity/Service/AuthenticationService.cs
+++ src/Wego/Infrastucture/Wego.Identity/Service/AuthenticationService.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 
 using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 
 using Wego.Application.Contracts.Identity;
 using Wego.Application.Exceptions;
