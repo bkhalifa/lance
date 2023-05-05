5f1d51a33142c80c8496e2778cdb9b17337a990e
Bessem Zbidi <zbidi.bessem@gmail.com>
2023-05-05 23:50:23 +0200

    Serilog problem details adjustments

--- src/Wego/Infrastucture/Wego.Persistence/EF/PortoDbContext.cs
+++ src/Wego/Infrastucture/Wego.Persistence/EF/PortoDbContext.cs
@@ -56,9 +56,6 @@ public partial class PortoDbContext : DbContext
 
     public virtual DbSet<ZipCode> ZipCodes { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=porto;Trusted_Connection=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- src/Wego/Infrastucture/Wego.Persistence/PersistenceServiceRegistration.cs
+++ src/Wego/Infrastucture/Wego.Persistence/PersistenceServiceRegistration.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using System.Configuration;
+using Wego.Application.Contracts.Common;
 using Wego.Application.Contracts.Persistence;
+using Wego.Application.Features.Common;
+using Wego.Application.Models.Mail;
 using Wego.Persistence.EF;
 using Wego.Persistence.Respositories;
 using Wego.Persistence.Respositories.Config;
@@ -19,7 +22,8 @@ public static class PersistenceServiceRegistration
         services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
 
         services.AddScoped<ICategoryRepository, CategoryRepository>();
-        
+        services.AddScoped<IEmailSender, EmailSender>();
+        services.Configure<EmailSettings>(configuration.GetSection(nameof(EmailSettings)));
 
         return services;
     }
